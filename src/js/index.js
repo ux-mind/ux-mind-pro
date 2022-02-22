3230fed10b438a009c83aecfcb895f6948c4f4cb
Pavel Surkov <surkovv.pavel@gmail.com>
2022-02-22 21:42:10 +0700

    adds correct styles folder

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
-import AppService from './modules/app.service.jsx';
-import { config } from './modules/config.js';
-import './modules/header.component.jsx';
-import '../scss/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../styles/main.scss';
 import '../index.html';
 
-console.log('2. config key: ', config.key);
+const App = () => {
+	return (
+		<div className="container">
+			<div className="jsx">JSX!!!</div>
+		</div>
+	);
+};
 
-const service = new AppService('3. Hello webpack');
-service.log();
-
-const a = 5;
+ReactDOM.render(<App />, document.querySelector('#root'))

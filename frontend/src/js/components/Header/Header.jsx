fd7ba5e3504b7c8fdd1441de84dbd63f4f29c040
Pavel Surkov <surkovv.pavel@gmail.com>
2022-12-08 17:41:13 +0700

    inits menu component

--- frontend/src/js/components/Header/Header.jsx
+++ frontend/src/js/components/Header/Header.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import Menu from './Menu';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/images/icons/logo.svg';
 
 const Header = () => {
 	return (
-		<header className="header">
-			<div className="container">
-				<div className="header-wrapper">
-					<Link className="logo header-logo" to="/">
-						<img width="38" height="38" src={logo} alt="logo" />
-					</Link>
-					<button type="button" className="menu-btn">
-						<div className="menu-btn-sticks">
-							<span></span>
-							<span></span>
-						</div>
-						<span className="menu-btn__text">Menu</span>
-					</button>
+		<>
+			<header className="header">
+				<div className="container">
+					<div className="header-wrapper">
+						<Link className="logo header-logo" to="/">
+							<img width="38" height="38" src={logo} alt="logo" />
+						</Link>
+						<button type="button" className="menu-btn">
+							<div className="menu-btn-sticks">
+								<span></span>
+								<span></span>
+							</div>
+							<span className="menu-btn__text">Menu</span>
+						</button>
+					</div>
 				</div>
-			</div>
-		</header>
+			</header>
+			<Menu />
+		</>
 	);
 };
 
---
+++ frontend/src/js/components/Header/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Menu = ({ opened }) => {
+	return (
+		<div className="menu">
+			<div className="container">
+				<div className="menu-wrapper"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Menu;

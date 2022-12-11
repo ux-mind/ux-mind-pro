df12db404d9fb41d31195961ceb66beb4d37eb1b
Pavel Surkov <surkovv.pavel@gmail.com>
2022-12-11 14:18:56 +0700

    adds experience section

---
+++ frontend/src/js/pages/Home/Experience/Experience.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Experience = () => {
+	return (
+		<section className="experience">
+			<div className="container">
+				<div className="experience-wrapper text_blue text_size-xl">
+					<p>
+						UX Mind Creative Agency's team with&nbsp;
+						<strong>7+&nbsp;years of experience</strong> in&nbsp;UX/UI
+						web&nbsp;&&nbsp;mobile design, NFT projects and SAAs. <br />
+						We create style.
+					</p>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Experience;
--- frontend/src/js/pages/Home/index.jsx
+++ frontend/src/js/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './Hero/Hero';
+import Experience from './Experience/Experience';
 
 const Home = () => {
 	return (
 		<div className="home">
 			<Hero />
+			<Experience />
 		</div>
 	);
 };

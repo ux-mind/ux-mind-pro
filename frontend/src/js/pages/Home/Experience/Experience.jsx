fb664da261ebbfdcf332126089a7ef2fb7aa38bf
Pavel Surkov <surkovv.pavel@gmail.com>
2022-12-13 19:03:36 +0700

    adds text animation (bugs)

--- frontend/src/js/pages/Home/Experience/Experience.jsx
+++ frontend/src/js/pages/Home/Experience/Experience.jsx
@@ -1,59 +1,73 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useTransform, motion, useScroll } from 'framer-motion';
-import { getViewportCoords, getCoords } from '../../../functions/functions';
+import { getCoords } from '../../../functions/functions';
 
 const Experience = () => {
 	const experienceRef = useRef(null);
-	const [offsetY, setOffsetY] = useState([0, 0, 0]);
+	const [offsetY, setOffsetY] = useState([0, 0, 0, 0]);
 
 	const { scrollY } = useScroll();
 
-	// Animation values for the Experience block
-	const topSizes = [0, 600, 850];
-	const heightSizes = [560, 1000, 0];
-	// const scale = [1, 1, 0];
-	const widthSizes = ['100%', '100%', '0%'];
-	const leftSizes = ['0%', '0%', '50%'];
+	// Animation values for the Experience text block
+	const topTextValues = [0, 600, 1000, 1600];
+	const heightTextValues = [560, 1000, 0, 0];
+	const widthTextValues = ['100%', '100%', '0%', '0%'];
+	const leftTextValues = ['0%', '0%', '50%', '50%'];
 
-	const minHeight = useTransform(scrollY, offsetY, heightSizes);
-	const topPosition = useTransform(scrollY, offsetY, topSizes);
-	// const scaleY = useTransform(scrollY, offsetY, scale);
-	const maxWidth = useTransform(scrollY, offsetY, widthSizes);
-	const leftPosition = useTransform(scrollY, offsetY, leftSizes);
+	const minHeight = useTransform(scrollY, offsetY, heightTextValues);
+	const topPosition = useTransform(scrollY, offsetY, topTextValues);
+	const maxWidth = useTransform(scrollY, offsetY, widthTextValues);
+	const leftPosition = useTransform(scrollY, offsetY, leftTextValues);
+
+	// Animation values for the Experience text
+	const textColorValues = ['#0D08FF', '#0D08FF', '#FFF', '#FFF'];
+
+	const textColor = useTransform(scrollY, offsetY, textColorValues);
+
+	// Animation values for the Experinece block
 
 	useEffect(() => {
 		if (experienceRef) {
-			const { top } = getCoords(experienceRef.current);
+			let { top } = getCoords(experienceRef.current);
 
-			const topValue = top - Math.max(0, Math.max(...topSizes));
+			top = top - 1600;
 
-			setOffsetY([topValue, topValue + 600, topValue + 1000]);
+			setOffsetY([top, top + 600, top + 1000, top + 1600]);
 		}
 	}, [experienceRef]);
 
+	// For controlling scroll value TODO: remove
+	useEffect(() => {
+		window.onscroll = () => {
+			console.log(scrollY.current);
+		};
+	}, []);
+
 	return (
-		<motion.div
-			className="experience"
-			ref={experienceRef}
-			style={{
-				height: minHeight,
-				marginTop: topPosition,
-				maxWidth: maxWidth,
-				left: leftPosition
-				// scale: scaleY
-			}}
-		>
-			<div className="container">
-				<div className="experience-wrapper text_blue text_size-xl">
-					<p>
-						UX Mind Creative Agency's team with&nbsp;
-						<strong>7+&nbsp;years of experience</strong> in&nbsp;UX/UI
-						web&nbsp;&&nbsp;mobile design, NFT projects and SAAs. <br />
-						We create style.
-					</p>
+		<div className="experience">
+			<motion.div
+				className="experience-bg"
+				ref={experienceRef}
+				style={{
+					height: minHeight,
+					marginTop: topPosition,
+					maxWidth: maxWidth,
+					left: leftPosition
+				}}
+			></motion.div>
+			<div className="experience-text">
+				<div className="container">
+					<div className="experience-text__wrapper text_blue text_size-xl">
+						<motion.p style={{ color: textColor }}>
+							UX Mind Creative Agency's team with&nbsp;
+							<strong>7+&nbsp;years of experience</strong> in&nbsp;UX/UI
+							web&nbsp;&&nbsp;mobile design, NFT projects and SAAs. <br />
+							We create style.
+						</motion.p>
+					</div>
 				</div>
 			</div>
-		</motion.div>
+		</div>
 	);
 };
 

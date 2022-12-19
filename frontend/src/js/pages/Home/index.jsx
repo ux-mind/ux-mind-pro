babce408d774141a2255f95788ba402e579d601d
Pavel Surkov <surkovv.pavel@gmail.com>
2022-12-19 17:44:26 +0700

    installs swiper

--- frontend/src/js/pages/Home/index.jsx
+++ frontend/src/js/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import Experience from './Experience/Experience';
 import Projects from './Projects/Projects';
 import Banner from './Banner/Banner';
 import Request from './Request/Request';
+import Customers from './Customers/Customers;';
 
 const Home = () => {
 	return (
@@ -13,6 +14,7 @@ const Home = () => {
 			<Projects />
 			<Banner />
 			<Request />
+			<Customers />
 		</div>
 	);
 };

ae4fdf573dc3d0f3e0be1c2405ef620078708488
Pavel Surkov <surkovv.pavel@gmail.com>
2022-12-24 02:10:51 +0700

    adds projectSlider

--- frontend/src/js/pages/Home/Projects/ProjectSlider/ProjectSlider.jsx
+++ frontend/src/js/pages/Home/Projects/ProjectSlider/ProjectSlider.jsx
@@ -1,36 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, EffectFade } from 'swiper';
 
 const ProjectSlider = ({ data }) => {
-	// TODO: ADD SLIDER
+	const paginationRef = useRef(null);
+
 	return (
 		<div className="latest-projects-slider">
 			<div className="latest-projects-slider__wrapper">
-				<div className="latest-projects-slider__content">
-					<div className="text-block">
-						<span className="chapter text_size-s">Ecommerce</span>
-						<h3 className="title title_size-sm">Headphone Store</h3>
-						<p>
-							All the Lorem Ipsum generators on the Internet tend to repeat predefined
-							chunks as necessary, making this the first true generator on the
-							Internet.
-						</p>
-					</div>
-				</div>
-				<div className="latest-projects-slider__image">
-					<img
-						width="835"
-						height="626"
-						src={data[0].img.x1}
-						srcSet={`${data[0].img.x1} 1x, ${data[0].img.x2} 2x`}
-						alt="project"
-					/>
-				</div>
+				<Swiper
+					modules={[EffectFade, Pagination]}
+					pagination={{
+						el: paginationRef.current
+					}}
+					onBeforeInit={(swiper) => {
+						swiper.params.pagination.el = paginationRef.current;
+					}}
+					effect="fade"
+				>
+					{data.map((project) => (
+						<SwiperSlide key={project.id}>
+							<div className="latest-projects-slider__image">
+								<img
+									width="835"
+									height="626"
+									src={project.img.x1}
+									srcSet={`${project.img.x1} 1x, ${project.img.x2} 2x`}
+									alt="project"
+								/>
+							</div>
+							<div className="latest-projects-slider__content">
+								<div className="text-block">
+									<span className="chapter text_size-s">{project.industry}</span>
+									<h3 className="title title_size-sm">{project.name}</h3>
+									<p>{project.content}</p>
+								</div>
+							</div>
+						</SwiperSlide>
+					))}
+				</Swiper>
 			</div>
-			<div className="latest-projects-slider__pagination">
-				<span className="pagination-tab pagination-tab_active"></span>
-				<span className="pagination-tab"></span>
-				<span className="pagination-tab"></span>
-			</div>
+			<div className="latest-projects-slider__pagination" ref={paginationRef}></div>
 		</div>
 	);
 };

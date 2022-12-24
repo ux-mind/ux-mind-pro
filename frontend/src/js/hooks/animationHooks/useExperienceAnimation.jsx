d28fc629a98682c95e77b2f96afc78af7b0f0bee
Pavel Surkov <surkovv.pavel@gmail.com>
2022-12-24 19:08:52 +0700

    adds useExperienceAnimation hook

---
+++ frontend/src/js/hooks/animationHooks/useExperienceAnimation.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+import { useTransform, useScroll } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
+import { getCoords } from '../../functions/functions';
+
+const useExperienceAnimation = (experienceRef) => {
+	const isMobile = useMediaQuery({
+		query: `(max-width: 991px)`
+	});
+
+	const { scrollY } = useScroll();
+
+	const [offsetY, setOffsetY] = useState(() => (isMobile ? [0, 0] : [0, 0, 0, 0, 0]));
+
+	// Animation values for the Experience block
+	const topValues = isMobile ? [0, 800] : [0, 800, 1600, 1800, 2100];
+	const heightValues = isMobile ? [375, 812] : [560, 1000, 1000, 1000, 1000];
+
+	const minHeight = useTransform(scrollY, offsetY, heightValues);
+	const topPosition = useTransform(scrollY, offsetY, topValues);
+
+	// Animation values for the Experience background
+	const heightBgValues = isMobile ? ['100%', '100%'] : ['100%', '100%', '0%', '0%', '0%'];
+	const widthBgValues = isMobile ? ['100%', '100%'] : ['100%', '100%', '0%', '0%', '0%'];
+	const leftBgValues = isMobile ? ['0%', '0%'] : ['0%', '0%', '50%', '50%', '50%'];
+
+	const heightBgPosition = useTransform(scrollY, offsetY, heightBgValues);
+	const maxBgWidth = useTransform(scrollY, offsetY, widthBgValues);
+	const leftBgPosition = useTransform(scrollY, offsetY, leftBgValues);
+
+	// Animation values for the Experience text
+	const textColorValues = isMobile
+		? ['#0D08FF', '#0D08FF']
+		: ['#0D08FF', '#0D08FF', '#FFF', '#FFF', 'rgba(255, 255, 255, 0)'];
+	const textTopValues = isMobile ? ['50%', '50%'] : ['50%', '50%', '50%', '50%', '0%'];
+	const textScaleValues = isMobile ? [1, 1] : [1, 1, 1, 1, 0.6];
+
+	const textColor = useTransform(scrollY, offsetY, textColorValues);
+	const textTop = useTransform(scrollY, offsetY, textTopValues);
+	const textScale = useTransform(scrollY, offsetY, textScaleValues);
+
+	const animationValuesLength = topValues.length;
+
+	useEffect(() => {
+		if (experienceRef) {
+			let { top } = getCoords(experienceRef.current);
+
+			top = top - topValues[topValues.length - 1];
+
+			let offsetArr = new Array(topValues.length).fill(top);
+
+			offsetArr = offsetArr.map((value, idx) => value + topValues[idx]);
+
+			setOffsetY(offsetArr);
+		}
+	}, [experienceRef]);
+
+	return {
+		minHeight,
+		topPosition,
+		heightBgPosition,
+		maxBgWidth,
+		leftBgPosition,
+		textColor,
+		textTop,
+		textScale,
+		animationValuesLength
+	};
+};
+
+export default useExperienceAnimation;
--- frontend/src/js/pages/Home/Experience/Experience.jsx
+++ frontend/src/js/pages/Home/Experience/Experience.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import { useTransform, motion, useScroll } from 'framer-motion';
-import useExperienceScroll from '../../../hooks/scrollHooks/useExperienceScroll';
-import { getCoords } from '../../../functions/functions';
+import { motion } from 'framer-motion';
+import useExperienceAnimation from '../../../hooks/animationHooks/useExperienceAnimation';
 
 const Experience = () => {
 	const isMobile = useMediaQuery({
@@ -11,71 +10,36 @@ const Experience = () => {
 
 	const experienceRef = useRef(null);
 
-	const [offsetY, setOffsetY] = useState(() => (isMobile ? [0, 0] : [0, 0, 0, 0, 0]));
-
-	const { scrollY } = useScroll();
-
-	// Animation values for the Experience block
-	const topValues = useExperienceScroll();
-	const heightValues = isMobile ? [375, 812] : [560, 1000, 1000, 1000, 1000];
-
-	const minHeight = useTransform(scrollY, offsetY, heightValues);
-	const topPosition = useTransform(scrollY, offsetY, topValues);
-
-	// Animation values for the Experience background
-	const heightBgValues = isMobile ? ['100%', '100%'] : ['100%', '100%', '0%', '0%', '0%'];
-	const widthBgValues = isMobile ? ['100%', '100%'] : ['100%', '100%', '0%', '0%', '0%'];
-	const leftBgValues = isMobile ? ['0%', '0%'] : ['0%', '0%', '50%', '50%', '50%'];
-
-	const heightBgPosition = useTransform(scrollY, offsetY, heightBgValues);
-	const maxBgWidth = useTransform(scrollY, offsetY, widthBgValues);
-	const leftBgPosition = useTransform(scrollY, offsetY, leftBgValues);
-
-	// Animation values for the Experience text
-	const textColorValues = isMobile
-		? ['#0D08FF', '#0D08FF']
-		: ['#0D08FF', '#0D08FF', '#FFF', '#FFF', 'rgba(255, 255, 255, 0)'];
-	const textTopValues = isMobile ? ['50%', '50%'] : ['50%', '50%', '50%', '50%', '0%'];
-	const textScaleValues = isMobile ? [1, 1] : [1, 1, 1, 1, 0.6];
-
-	const textColor = useTransform(scrollY, offsetY, textColorValues);
-	const textTop = useTransform(scrollY, offsetY, textTopValues);
-	const textScale = useTransform(scrollY, offsetY, textScaleValues);
-
-	useEffect(() => {
-		if (experienceRef) {
-			let { top } = getCoords(experienceRef.current);
-
-			top = top - topValues[topValues.length - 1];
-
-			let offsetArr = new Array(topValues.length).fill(top);
-
-			offsetArr = offsetArr.map((value, idx) => value + topValues[idx]);
-
-			setOffsetY(offsetArr);
-		}
-	}, [experienceRef, isMobile]);
+	const animationValues = useExperienceAnimation(experienceRef);
 
 	return (
 		<motion.div
 			className="experience"
 			ref={experienceRef}
 			style={{
-				marginTop: topPosition,
-				height: minHeight
+				marginTop: animationValues.topPosition,
+				height: animationValues.minHeight
 			}}
 		>
 			<motion.div
 				className="experience-bg"
-				style={{ maxWidth: maxBgWidth, height: heightBgPosition, left: leftBgPosition }}
+				style={{
+					maxWidth: animationValues.maxBgWidth,
+					height: animationValues.heightBgPosition,
+					left: animationValues.leftBgPosition
+				}}
 			></motion.div>
 			<motion.div
 				className="experience-text"
-				style={{ top: textTop, scale: textScale, translateY: '-50%' }}
+				style={{
+					top: animationValues.textTop,
+					scale: animationValues.textScale,
+					translateY: '-50%'
+				}}
 			>
 				<div className="container">
 					<div className="experience-text__wrapper text_blue text_size-xl">
-						<motion.p style={{ color: textColor }}>
+						<motion.p style={{ color: animationValues.textColor }}>
 							UX Mind Creative Agency's team with&nbsp;
 							<strong>7+&nbsp;years of experience</strong> in&nbsp;UX/UI
 							web&nbsp;&&nbsp;mobile design, NFT projects and SAAs. <br />

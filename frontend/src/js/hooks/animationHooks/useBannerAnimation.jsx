add9eba0d6e267263dcecbe4e43e62f4f601bde1
Pavel Surkov <surkovv.pavel@gmail.com>
2022-12-28 21:06:53 +0700

    creates useBannerAnimation hook

---
+++ frontend/src/js/hooks/animationHooks/useBannerAnimation.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+import { getCoords, getViewportCoords } from '../../functions/functions';
+import { useTransform, useScroll } from 'framer-motion';
+
+const useBannerAnimation = () => {
+	const { scrollY } = useScroll();
+
+	const [banner, setBanner] = useState(null);
+	const [bannerWrapper, setBannerWrapper] = useState(null);
+
+	const [bannerShown, setBannerShown] = useState(0);
+
+	const [offsetY, setOffsetY] = useState(() => [0, 0]);
+
+	const topValues = [0, 1200];
+	const heightValues = ['54.4%', '100%'];
+
+	const topPosition = useTransform(scrollY, offsetY, topValues);
+	const minHeight = useTransform(scrollY, offsetY, heightValues);
+
+	useEffect(() => {
+		const banner = document.querySelector('#banner');
+		const bannerWrapper = document.querySelector('#banner-wrapper');
+
+		if (banner) {
+			setBanner(banner);
+		}
+
+		if (bannerWrapper) {
+			setBannerWrapper(bannerWrapper);
+		}
+	}, []);
+
+	useEffect(() => {
+		if (bannerWrapper) {
+			const viewportHeight = window.innerHeight;
+
+			window.addEventListener('scroll', () => {
+				const { top } = getViewportCoords(bannerWrapper);
+
+				const bannerShown = -top + viewportHeight >= bannerWrapper.offsetHeight / 4;
+
+				setBannerShown(bannerShown);
+			});
+		}
+
+		return () => {
+			window.removeEventListener('scroll', () => {
+				const { top } = getViewportCoords(bannerWrapper);
+
+				const bannerShown = -top + window.innerHeight >= bannerWrapper.offsetHeight / 4;
+
+				setBannerShown(bannerShown);
+			});
+		};
+	}, [bannerWrapper]);
+
+	useEffect(() => {
+		if (banner) {
+			let { top } = getCoords(banner);
+
+			top = top - topValues[topValues.length - 1];
+
+			let offsetArr = new Array(topValues.length).fill(top);
+
+			offsetArr = offsetArr.map((value, idx) => value + topValues[idx]);
+
+			setOffsetY(offsetArr);
+		}
+	}, [banner]);
+
+	return { bannerShown, topPosition, minHeight };
+};
+
+export default useBannerAnimation;
--- frontend/src/js/pages/Home/Banner/Banner.jsx
+++ frontend/src/js/pages/Home/Banner/Banner.jsx
@@ -1,84 +1,24 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { useInView, motion, useScroll, useTransform } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import { getViewportCoords, getCoords } from '../../../functions/functions';
+import useBannerAnimation from '../../../hooks/animationHooks/useBannerAnimation';
 
 import banner from '../../../../assets/images/home-banner.png';
 import banner2x from '../../../../assets/images/home-banner@2x.png';
 
 const Banner = () => {
-	const bannerRef = useRef(null);
-	const bannerWrapperRef = useRef(null);
-
-	const isInView = useInView(bannerRef);
-
-	const { scrollY } = useScroll();
-
-	const [bannerShown, setBannerShown] = useState(0);
-
-	const [offsetY, setOffsetY] = useState(() => [0, 0]);
-
-	const topValues = [0, 1200];
-	const heightValues = ['54.4%', '100%'];
-
-	const topPosition = useTransform(scrollY, offsetY, topValues);
-	const minHeight = useTransform(scrollY, offsetY, heightValues);
-
-	useEffect(() => {
-		if (bannerWrapperRef) {
-			const banner = bannerWrapperRef.current;
-
-			const viewportHeight = window.innerHeight;
-
-			window.addEventListener('scroll', () => {
-				const { top } = getViewportCoords(banner);
-
-				console.log(window.scrollY);
-
-				const bannerShown = -top + viewportHeight >= banner.offsetHeight / 4;
-
-				setBannerShown(bannerShown);
-			});
-		}
-
-		return () => {
-			window.removeEventListener('scroll', () => {
-				const { top } = getViewportCoords(bannerWrapperRef.current);
-
-				const bannerShown =
-					-top + window.innerHeight >= bannerWrapperRef.current.offsetHeight / 4;
-
-				setBannerShown(bannerShown);
-			});
-		};
-	}, [bannerWrapperRef]);
-
-	useEffect(() => {
-		if (bannerRef) {
-			const bannerBlock = bannerRef.current;
-
-			let { top } = getCoords(bannerBlock);
-
-			top = top - topValues[topValues.length - 1];
-
-			let offsetArr = new Array(topValues.length).fill(top);
-
-			offsetArr = offsetArr.map((value, idx) => value + topValues[idx]);
-
-			setOffsetY(offsetArr);
-		}
-	}, [bannerRef]);
+	const { bannerShown, topPosition, minHeight } = useBannerAnimation();
 
 	return (
 		<motion.div
 			id="banner"
 			className="home-banner"
-			ref={bannerRef}
 			style={{ opacity: bannerShown ? '1' : '0', transition: 'opacity 1s' }}
 		>
 			<motion.div className="home-banner__block" style={{ marginTop: topPosition }}>
 				<motion.div
+					id="banner-wrapper"
 					className="home-banner__wrapper"
-					ref={bannerWrapperRef}
 					style={{ height: minHeight }}
 				>
 					<img

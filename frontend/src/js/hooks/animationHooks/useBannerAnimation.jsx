b6a4b72ae03f35ae8bf292fa9ed511b1da24e9cd
Pavel Surkov <surkovv.pavel@gmail.com>
2022-12-29 21:43:46 +0700

    removes body scrollbar

--- frontend/src/js/hooks/animationHooks/useBannerAnimation.jsx
+++ frontend/src/js/hooks/animationHooks/useBannerAnimation.jsx
@@ -8,23 +8,22 @@ const useBannerAnimation = () => {
 		query: `(max-width: 991px)`
 	});
 
-	const { scrollY } = useScroll();
-
 	const [banner, setBanner] = useState(null);
 	const [bannerWrapper, setBannerWrapper] = useState(null);
 
-	const [bannerShown, setBannerShown] = useState(0);
+	const { scrollY } = useScroll();
 
 	const [offsetY, setOffsetY] = useState(() => [0, 0]);
 
+	const [bannerShown, setBannerShown] = useState(0);
+
+	// animation values for the banner block
 	const topValues = [0, 1200];
 	const heightValues = isMobile ? ['100%', '100%'] : ['54.4%', '100%'];
+	const mobileHeightValues = isMobile ? [210, 811] : null;
 
 	const topPosition = useTransform(scrollY, offsetY, topValues);
 	const minHeight = useTransform(scrollY, offsetY, heightValues);
-
-	const mobileHeightValues = isMobile ? [210, 811] : null;
-
 	const mobileHeight = mobileHeightValues
 		? useTransform(scrollY, offsetY, mobileHeightValues)
 		: null;
@@ -41,6 +40,20 @@ const useBannerAnimation = () => {
 			setBannerWrapper(bannerWrapper);
 		}
 	}, []);
+
+	useEffect(() => {
+		if (banner) {
+			let { top } = getCoords(banner);
+
+			top = top - topValues[topValues.length - 1];
+
+			let offsetArr = new Array(topValues.length).fill(top);
+
+			offsetArr = offsetArr.map((value, idx) => value + topValues[idx]);
+
+			setOffsetY(offsetArr);
+		}
+	}, [banner]);
 
 	useEffect(() => {
 		if (bannerWrapper) {
@@ -65,20 +78,6 @@ const useBannerAnimation = () => {
 			});
 		};
 	}, [bannerWrapper]);
-
-	useEffect(() => {
-		if (banner) {
-			let { top } = getCoords(banner);
-
-			top = top - topValues[topValues.length - 1];
-
-			let offsetArr = new Array(topValues.length).fill(top);
-
-			offsetArr = offsetArr.map((value, idx) => value + topValues[idx]);
-
-			setOffsetY(offsetArr);
-		}
-	}, [banner]);
 
 	return { bannerShown, topPosition, minHeight, mobileHeight };
 };

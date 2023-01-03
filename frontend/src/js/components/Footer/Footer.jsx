bdd6414d31144c68a2d4d78554fc80fecfdc98f8
maksimvasilevskiy <97239271+maksimvasilevskiy@users.noreply.github.com>
2023-01-03 19:40:12 +0300

    footer data loaded

--- frontend/src/js/components/Footer/Footer.jsx
+++ frontend/src/js/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import logo from '../../../assets/images/icons/logo.svg';
 import PUBLIC_ROUTES from '../../data/publicRoutes';
 import SOCIAL_LINKS from '../../data/socialLinks';
+import useFetch from '../../hooks/useFetch';
 
 const menuLinks = [
 	{ name: 'About', route: PUBLIC_ROUTES.about },
@@ -20,56 +21,66 @@ const socialLinks = [
 ];
 
 const Footer = () => {
-	return (
-		<footer className="footer">
-			<div className="container">
-				<div className="footer-wrapper">
-					<div className="footer-top">
-						<div className="footer-logo">
-							<Link className="logo footer-logo__link" to="/">
-								<img width="38" height="38" src={logo} alt="logo" />
-							</Link>
-						</div>
-						<nav className="navigation footer-navigation">
-							<ul className="footer-list">
-								{menuLinks.map(({ name, route }) => (
-									<li className="footer-list__item" key={name}>
-										<Link className="link footer-list__link" to={route}>
-											{name}
-										</Link>
-									</li>
-								))}
-							</ul>
-							<ul className="footer-list">
-								{socialLinks.map(({ name, route }) => (
-									<li className="footer-list__item" key={name}>
-										<Link className="link footer-list__link" to={route}>
-											{name}
-										</Link>
-									</li>
-								))}
-							</ul>
-						</nav>
-						<div className="footer-contacts">
-							<div className="footer-contacts__link">
-								<a className="link" href="mailto:info@uxmindpro.com">
-									info@uxmindpro.com
-								</a>
+	const data = useFetch(
+		'http://localhost:1337/api/footer?&populate=%2A',
+		{}
+	);
+
+	if (data.data) {
+		const attributes = data.data.data.attributes;
+		return (
+			<footer className="footer">
+				<div className="container">
+					<div className="footer-wrapper">
+						<div className="footer-top">
+							<div className="footer-logo">
+								<Link className="logo footer-logo__link" to="/">
+									<img width="38" height="38" src={'http://localhost:1337' + attributes.footer_logo.data.attributes.url} alt="logo" />
+								</Link>
 							</div>
-							<div className="footer-contacts__link">
-								<a className="link" href="tel:+00000000000">
-									+00 000 000 000
-								</a>
+							<nav className="navigation footer-navigation">
+								<ul className="footer-list">
+									{attributes.footer_menu.map(({ text, link }) => (
+										<li className="footer-list__item" key={text}>
+											<Link className="link footer-list__link" to={link}>
+												{text}
+											</Link>
+										</li>
+									))}
+								</ul>
+								<ul className="footer-list">
+									{attributes.footer_menu_social.map(({ text, link }) => (
+										<li className="footer-list__item" key={text}>
+											<Link className="link footer-list__link" to={link}>
+												{text}
+											</Link>
+										</li>
+									))}
+								</ul>
+							</nav>
+							<div className="footer-contacts">
+								<div className="footer-contacts__link">
+									<a className="link" href={`mailto:${attributes.footer_mail}`}>
+										{attributes.footer_mail}
+									</a>
+								</div>
+								<div className="footer-contacts__link">
+									<a className="link" href={`tel:${attributes.footer_phone}`}>
+										{attributes.footer_phone}
+									</a>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="footer-bottom">
-						<p>2022 UX Mind. — Design Agency</p>
+						<div className="footer-bottom">
+							<p>{attributes.footer_credits}</p>
+						</div>
 					</div>
 				</div>
-			</div>
-		</footer>
-	);
+			</footer>
+		);
+	} else {
+		return '';
+	}
 };
 
 export default Footer;

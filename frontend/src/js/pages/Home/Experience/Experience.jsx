6653c50419dfb3b735d6354bf3f5b474f1e36863
maksimvasilevskiy <97239271+maksimvasilevskiy@users.noreply.github.com>
2023-01-03 10:51:21 +0300

    experience data loaded

--- frontend/src/js/pages/Home/Experience/Experience.jsx
+++ frontend/src/js/pages/Home/Experience/Experience.jsx
@@ -2,45 +2,43 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import useExperienceAnimation from '../../../hooks/animationHooks/useExperienceAnimation';
 
-const Experience = () => {
+const Experience = ({ attributes }) => {
 	const animationValues = useExperienceAnimation();
 
-	return (
-		<motion.div
-			className="experience"
-			id="experience"
-			style={{
-				marginTop: animationValues.topPosition,
-				height: animationValues.minHeight
-			}}
-		>
+	if ({ attributes }) {
+		return (
 			<motion.div
-				className="experience-bg"
+				className="experience"
+				id="experience"
 				style={{
-					height: animationValues.heightBgPosition
-				}}
-			></motion.div>
-			<motion.div
-				className="experience-text"
-				style={{
-					top: animationValues.textTop,
-					scale: animationValues.textScale,
-					translateY: '-50%'
+					marginTop: animationValues.topPosition,
+					height: animationValues.minHeight
 				}}
 			>
-				<div className="container">
-					<div className="experience-text__wrapper text_blue text_size-xl">
-						<motion.p style={{ color: animationValues.textColor }}>
-							UX Mind Creative Agency's team with&nbsp;
-							<strong>7+&nbsp;years of experience</strong> in&nbsp;UX/UI
-							web&nbsp;&&nbsp;mobile design, NFT projects and SAAs. <br />
-							We create style.
-						</motion.p>
+				<motion.div
+					className="experience-bg"
+					style={{
+						height: animationValues.heightBgPosition
+					}}
+				></motion.div>
+				<motion.div
+					className="experience-text"
+					style={{
+						top: animationValues.textTop,
+						scale: animationValues.textScale,
+						translateY: '-50%'
+					}}
+				>
+					<div className="container">
+						<div className="experience-text__wrapper text_blue text_size-xl">
+							<motion.p style={{ color: animationValues.textColor }} dangerouslySetInnerHTML={{__html: attributes.experience_text}}>
+							</motion.p>
+						</div>
 					</div>
-				</div>
+				</motion.div>
 			</motion.div>
-		</motion.div>
-	);
+		);
+	}
 };
 
 export default Experience;

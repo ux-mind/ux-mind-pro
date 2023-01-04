d2bd5cbf79229bfcf15a2e6f254d247abafeb95c
Pavel Surkov <surkovv.pavel@gmail.com>
2023-01-04 20:40:49 +0700

    adds smooth scroll

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Head from './components/Head';
-// import { ScrollerMotion } from 'scroller-motion';
+import { ScrollerMotion } from 'scroller-motion';
 
 const App = () => {
 	return (
 		<React.StrictMode>
 			<Head />
-			<div className="app">
-				{/* <ScrollerMotion > */}
+			<div className="app" style={{ scrollBehavior: 'unset' }}>
+				{/* <ScrollerMotion disabled={false} spring={{ damping: 0.1 }}> */}
 				<BrowserRouter>
 					<Header />
 					<Routes>

5f4fabcfa1a09b02a40d8c29ce38fbccdd5b268f
Pavel Surkov <surkovv.pavel@gmail.com>
2023-01-05 23:36:55 +0700

    adds smooth animation for menu

--- frontend/src/js/components/Header/Menu.jsx
+++ frontend/src/js/components/Header/Menu.jsx
@@ -15,54 +15,114 @@ const menuLinks = [
 ];
 
 const Menu = ({ opened }) => {
+	const menuVariants = {
+		hidden: {
+			translateX: '100%'
+		},
+		show: {
+			translateX: '0%'
+		}
+	};
+
+	const innerVariants = {
+		hidden: {
+			opacity: 0,
+			translateX: '-40%'
+		},
+		show: {
+			opacity: 1,
+			translateX: '0%'
+		}
+	};
+
 	return (
-		<motion.div className={opened ? 'menu menu_opened' : 'menu'}>
-			<Scrollbars>
-				<div className="menu-wrapper">
-					<div className="container">
-						<div className="menu-content">
-							<nav className="nav menu-nav">
-								<ul className="menu-list">
-									{menuLinks.map((link) => {
-										return (
-											<li className="menu-list__item" key={link.name}>
-												<Link
-													to={link.route}
-													className="title title_size-s link"
-												>
-													{link.name}
-												</Link>
-											</li>
-										);
-									})}
-								</ul>
-							</nav>
-							<div className="menu-contacts">
-								<div className="menu-contacts__link-wrapper">
-									<a
-										href={`mailto:${CONTACT.email}`}
-										className="title title_size-xs link"
+		<AnimatePresence>
+			{opened ? (
+				<motion.div
+					className={opened ? 'menu menu_opened' : 'menu'}
+					variants={menuVariants}
+					initial="hidden"
+					animate="show"
+					exit="hidden"
+					transition={{ ease: 'easeOut', duration: 0.6 }}
+				>
+					<Scrollbars>
+						<div className="menu-wrapper">
+							<div className="container">
+								<div className="menu-content">
+									<motion.nav
+										className="nav menu-nav"
+										variants={innerVariants}
+										initial="hidden"
+										animate="show"
+										exit="hidden"
+										transition={{
+											ease: 'easeIn',
+											duration: 0.55
+										}}
 									>
-										{CONTACT.email}
-									</a>
-								</div>
-								<div className="menu-contacts__link-wrapper">
-									<a
-										href={`tel:${CONTACT.tel}`}
-										className="title title_size-xs link"
+										<ul className="menu-list">
+											{menuLinks.map((link) => {
+												return (
+													<li className="menu-list__item" key={link.name}>
+														<Link
+															to={link.route}
+															className="title title_size-s link"
+														>
+															{link.name}
+														</Link>
+													</li>
+												);
+											})}
+										</ul>
+									</motion.nav>
+									<motion.div
+										className="menu-contacts"
+										variants={innerVariants}
+										initial="hidden"
+										animate="show"
+										exit="hidden"
+										transition={{
+											ease: 'easeIn',
+											duration: 0.55
+										}}
 									>
-										{CONTACT.tel}
-									</a>
+										<div className="menu-contacts__link-wrapper">
+											<a
+												href={`mailto:${CONTACT.email}`}
+												className="title title_size-xs link"
+											>
+												{CONTACT.email}
+											</a>
+										</div>
+										<div className="menu-contacts__link-wrapper">
+											<a
+												href={`tel:${CONTACT.tel}`}
+												className="title title_size-xs link"
+											>
+												{CONTACT.tel}
+											</a>
+										</div>
+									</motion.div>
 								</div>
 							</div>
+							<motion.div
+								className="menu-textline"
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								exit={{ opacity: 0 }}
+								transition={{
+									ease: 'easeIn',
+									duration: 0.6
+								}}
+							>
+								<AnimatedTextLine />
+							</motion.div>
 						</div>
-					</div>
-					<div className="menu-textline">
-						<AnimatedTextLine />
-					</div>
-				</div>
-			</Scrollbars>
-		</motion.div>
+					</Scrollbars>
+				</motion.div>
+			) : null}
+		</AnimatePresence>
 	);
 };
 

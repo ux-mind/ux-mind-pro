ba5f4126bf4ccf1f9369bf9cf61e006eb3c47ee0
Pavel Surkov <surkovv.pavel@gmail.com>
2023-01-30 19:18:52 +0700

    adds scrollTrigger

--- frontend/src/js/hooks/animationHooks/useExperienceAnimation.jsx
+++ frontend/src/js/hooks/animationHooks/useExperienceAnimation.jsx
@@ -8,118 +8,135 @@ import ScrollTrigger from 'gsap-trial/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const useExperienceAnimation = (context) => {
+const useExperienceAnimation = () => {
 	const isMobile = useMediaQuery({
 		query: `(max-width: 991px)`
 	});
 
 	useLayoutEffect(() => {
+		// gsap.to('#experience', {
+		// 	scrollTrigger: {
+		// 		trigger: '#experience',
+		// 		start: 'top top',
+		// 		end: 'bottom 150px',
+		// 		scrub: 1,
+		// 		markers: true,
+		// 		pin: true
+		// 	}
+		// 	// y: 300,
+		// 	// ease: 'none'
+		// });
+
 		ScrollTrigger.create({
 			trigger: '#experience',
+			pin: true,
 			start: 'top top',
 			end: '+=300px',
-			pin: true
+			markers: true,
+			scrub: 1
 		});
 	}, []);
 
+	return {};
+
 	// Old animation
 
-	const scrollY = useMotionValue(context.y);
+	// const scrollY = useMotionValue(context.y);
 
-	const [experienceBlock, setExperienceBlock] = useState(null);
-	const [experienceText, setExperienceText] = useState(null);
-	const [offsetY, setOffsetY] = useState([0, 0, 0]);
-	const viewportHeight = window.innerHeight;
+	// const [experienceBlock, setExperienceBlock] = useState(null);
+	// const [experienceText, setExperienceText] = useState(null);
+	// const [offsetY, setOffsetY] = useState([0, 0, 0]);
+	// const viewportHeight = window.innerHeight;
 
-	const maxScroll = viewportHeight * 2;
+	// const maxScroll = viewportHeight * 2;
 
-	// Declare static initial height of the white block;
-	const textPaddings = isMobile ? 130 : 144;
-	const initialBgHeight = experienceText ? experienceText.offsetHeight + textPaddings * 2 : 560;
+	// // Declare static initial height of the white block;
+	// const textPaddings = isMobile ? 130 : 144;
+	// const initialBgHeight = experienceText ? experienceText.offsetHeight + textPaddings * 2 : 560;
 
-	// Animation values for the Experience block
-	const offsetValues = [0, maxScroll / 2, maxScroll];
-	const topValues = [-maxScroll / 2, 0, 0];
+	// // Animation values for the Experience block
+	// const offsetValues = [0, maxScroll / 2, maxScroll];
+	// const topValues = [-maxScroll / 2, 0, 0];
 
-	const topPosition = useTransform(scrollY, offsetY, topValues);
+	// const topPosition = useTransform(scrollY, offsetY, topValues);
 
-	// Animation values for the Experience background
-	const experienceBgHeight = viewportHeight + 10;
-	const initialExperienceBgHeight = viewportHeight - initialBgHeight;
+	// // Animation values for the Experience background
+	// const experienceBgHeight = viewportHeight + 10;
+	// const initialExperienceBgHeight = viewportHeight - initialBgHeight;
 
-	const heightBgValues = [-initialExperienceBgHeight, 0, 0];
+	// const heightBgValues = [-initialExperienceBgHeight, 0, 0];
 
-	const heightBgPosition = useTransform(scrollY, offsetY, heightBgValues);
+	// const heightBgPosition = useTransform(scrollY, offsetY, heightBgValues);
 
-	// Animation values for the Experience text
-	const textTop = '50%';
-	const textPositionValues = [-initialExperienceBgHeight / 2, 0, 0];
+	// // Animation values for the Experience text
+	// const textTop = '50%';
+	// const textPositionValues = [-initialExperienceBgHeight / 2, 0, 0];
 
-	const textPosition = useTransform(scrollY, offsetY, textPositionValues);
+	// const textPosition = useTransform(scrollY, offsetY, textPositionValues);
 
-	// Animation values for the Projects block
-	const projectsOpacityValues = [0, 0, 3];
-	const projectsTranslateYValues = [
-		-maxScroll,
-		-maxScroll + (viewportHeight + initialExperienceBgHeight),
-		0
-	];
+	// // Animation values for the Projects block
+	// const projectsOpacityValues = [0, 0, 3];
+	// const projectsTranslateYValues = [
+	// 	-maxScroll,
+	// 	-maxScroll + (viewportHeight + initialExperienceBgHeight),
+	// 	0
+	// ];
 
-	const projectsOpacity = useTransform(scrollY, offsetY, projectsOpacityValues);
-	const projectsTranslateY = useTransform(scrollY, offsetY, projectsTranslateYValues);
+	// const projectsOpacity = useTransform(scrollY, offsetY, projectsOpacityValues);
+	// const projectsTranslateY = useTransform(scrollY, offsetY, projectsTranslateYValues);
 
-	const maxScrollValue = topValues.reduce((maxValue, value) => {
-		return Math.max(maxValue, value);
-	}, 0);
+	// const maxScrollValue = topValues.reduce((maxValue, value) => {
+	// 	return Math.max(maxValue, value);
+	// }, 0);
 
-	// Get elements to calculate animation values
-	useEffect(() => {
-		const experienceBlock = document.querySelector('#experience');
+	// // Get elements to calculate animation values
+	// useEffect(() => {
+	// 	const experienceBlock = document.querySelector('#experience');
 
-		if (experienceBlock) {
-			setExperienceBlock(experienceBlock);
-		}
+	// 	if (experienceBlock) {
+	// 		setExperienceBlock(experienceBlock);
+	// 	}
 
-		const experienceText = document.querySelector('#experience-text p');
+	// 	const experienceText = document.querySelector('#experience-text p');
 
-		if (experienceText) {
-			setExperienceText(experienceText);
-		}
-	}, []);
+	// 	if (experienceText) {
+	// 		setExperienceText(experienceText);
+	// 	}
+	// }, []);
 
-	useEffect(() => {
-		if (experienceBlock) {
-			let { top } = getCoords(experienceBlock);
+	// useEffect(() => {
+	// 	if (experienceBlock) {
+	// 		let { top } = getCoords(experienceBlock);
 
-			top = top - offsetValues[offsetValues.length - 2];
+	// 		top = top - offsetValues[offsetValues.length - 2];
 
-			console.log(top);
+	// 		console.log(top);
 
-			let offsetArr = new Array(offsetValues.length).fill(top);
+	// 		let offsetArr = new Array(offsetValues.length).fill(top);
 
-			offsetArr = offsetArr.map((value, idx) => value + offsetValues[idx]);
+	// 		offsetArr = offsetArr.map((value, idx) => value + offsetValues[idx]);
 
-			setOffsetY(offsetArr);
-		}
-	}, [experienceBlock]);
+	// 		setOffsetY(offsetArr);
+	// 	}
+	// }, [experienceBlock]);
 
-	useEffect(() => {
-		scrollY.set(context.y);
-	}, [context]);
+	// useEffect(() => {
+	// 	scrollY.set(context.y);
+	// }, [context]);
 
-	return {
-		offsetY,
-		maxScroll,
-		topPosition,
-		experienceBgHeight,
-		heightBgPosition,
-		projectsTranslateY,
-		projectsOpacity,
-		textTop,
-		textPosition,
-		animationValuesLength: offsetY.length,
-		maxScrollValue
-	};
+	// return {
+	// 	offsetY,
+	// 	maxScroll,
+	// 	topPosition,
+	// 	experienceBgHeight,
+	// 	heightBgPosition,
+	// 	projectsTranslateY,
+	// 	projectsOpacity,
+	// 	textTop,
+	// 	textPosition,
+	// 	animationValuesLength: offsetY.length,
+	// 	maxScrollValue
+	// };
 };
 
 export default useExperienceAnimation;
--- frontend/src/js/pages/Home/Experience/Experience.jsx
+++ frontend/src/js/pages/Home/Experience/Experience.jsx
@@ -1,56 +1,43 @@
-import React, { useRef } from 'react';
-import { motion } from 'framer-motion';
+import React, { useLayoutEffect } from 'react';
 import useExperienceAnimation from '../../../hooks/animationHooks/useExperienceAnimation';
 
-import { useSelector } from 'react-redux';
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap-trial/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Experience = () => {
-	const scroll = useSelector((state) => state.scroll.scrollValues);
+	// useExperienceAnimation();
 
-	const animationValues = useExperienceAnimation(scroll);
+	useLayoutEffect(() => {
+		ScrollTrigger.create({
+			trigger: '#experience',
+			pin: true,
+			start: 'top top',
+			end: '+=200px',
+			markers: true,
+			scrub: 1
+		});
+	}, []);
 
 	return (
-		<motion.div
-			className="experience"
-			id="experience"
-			style={{
-				marginTop: animationValues.maxScroll / 2,
-				translateY: animationValues.topPosition
-			}}
-		>
-			<motion.div
-				className="experience-bg"
-				style={{
-					height: animationValues.experienceBgHeight,
-					translateY: animationValues.heightBgPosition
-				}}
-			></motion.div>
-			<motion.div
-				className="experience-text"
-				style={{
-					top: animationValues.textTop,
-					translateY: '-50%'
-				}}
-			>
+		<div className="experience" id="experience">
+			<div className="experience-text">
 				<div className="container">
 					<div
 						id="experience-text"
 						className="experience-text__wrapper text_blue text_size-xl"
 					>
-						<motion.p
-							style={{
-								translateY: animationValues.textPosition
-							}}
-						>
+						<p>
 							UX Mind Creative Agency's team with&nbsp;
 							<strong>7+&nbsp;years of experience</strong> in&nbsp;UX/UI
 							web&nbsp;&&nbsp;mobile design, NFT projects and SAAs. <br />
 							We create style.
-						</motion.p>
+						</p>
 					</div>
 				</div>
-			</motion.div>
-		</motion.div>
+			</div>
+		</div>
 	);
 };
 
